74c8dcaed0d55b28e7227fdedb0c9e4207a81819
John Skweres <john.skweres@eagles.oc.edu>
2026-02-03 16:58:02 -0600

    Add areas tabel to the database

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,7 @@ import AuthRoutes from "./auth.routes.js";
 import UserRoutes from "./user.routes.js";
 import TutorialRoutes from "./tutorial.routes.js";
 import LessonRoutes from "./lesson.routes.js";
+import AreaRoutes from "./area.routes.js";
 
 
 const router = Router();
@@ -12,5 +13,6 @@ router.use("/", AuthRoutes);
 router.use("/users", UserRoutes);
 router.use("/tutorials", TutorialRoutes);
 router.use("/tutorials", LessonRoutes);
+router.use("/areas", AreaRoutes);
 
 export default router;
