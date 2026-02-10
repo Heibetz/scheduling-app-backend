7e1bab56de00a2978b77975d560ace525d6e2dd4
Hank Heiselbetz <hheiselbetz@gmail.com>
2026-02-10 16:59:00 -0600

    remove files

--- app/models/index.js
+++ app/models/index.js
@@ -6,7 +6,6 @@ import sequelize from "../config/sequelizeInstance.js";
 
 import User from "./user.model.js";
 import Session from "./session.model.js";
-import Tutorial from "./tutorial.model.js";
 import Availability from "./availability.model.js"; 
 import Position from "./position.model.js";
 import PositionUser from "./position_user.model.js";
@@ -19,8 +18,10 @@ db.sequelize = sequelize;
 
 db.user = User;
 db.session = Session;
-db.tutorial = Tutorial;
 db.availability = Availability;
+db.position = Position;
+db.positionUser = PositionUser;
+db.area = Area;
 
 // foreign key for session
 db.user.hasMany(db.session, { 
@@ -32,29 +33,6 @@ db.session.belongsTo(db.user, {
   as: "user",
   foreignKey: "user_id"
 });
-
-// foreign key for tutorials
-db.user.hasMany(db.tutorial, { 
-  as: "tutorial",
-  foreignKey: "user_id",
-  onDelete: "CASCADE"
-});
-db.tutorial.belongsTo(db.user, { 
-  as: "user",
-  foreignKey: "user_id"
-});
-
-// foreign key for lessons
-db.tutorial.hasMany(
-  db.lesson,
-  { as: "lesson" },
-  { foreignKey: { allowNull: false }, onDelete: "CASCADE" }
-);
-db.lesson.belongsTo(
-  db.tutorial,
-  { as: "tutorial" },
-  { foreignKey: { allowNull: false }, onDelete: "CASCADE" }
-);
 
 // foreign key for availability
 db.user.hasMany(db.availability, { 
