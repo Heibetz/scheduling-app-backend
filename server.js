073ddb5d6a919cb8a17fb2e015807ddb4d7f3ea0
Hank Heiselbetz <hheiselbetz@gmail.com>
2026-04-20 15:04:28 -0500

    add alter

--- server.js
+++ server.js
@@ -6,7 +6,17 @@ import morgan from "morgan";
 import db  from "./app/models/index.js";
 import logger from "./app/config/logger.js";
 
-db.sequelize.sync();
+db.sequelize.sync().then(async () => {
+  try {
+    await db.sequelize.query(
+      "ALTER TABLE `schedule_template` ADD COLUMN `duration_weeks` INT NOT NULL DEFAULT 1",
+      { raw: true }
+    );
+    logger.info("Added duration_weeks column to schedule_template");
+  } catch (e) {
+    if (e.original?.code !== 'ER_DUP_FIELDNAME') throw e;
+  }
+});
 
 const app = express();
 
